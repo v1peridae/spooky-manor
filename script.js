021810595edb12df34036e91170181e63832c4a4
Louisa Mulei <louisakavindu@gmail.com>
2024-10-23 19:15:13 +0300

    luh final changes

--- script.js
+++ script.js
@@ -7,3 +7,16 @@ var interval = window.setInterval(function () {
     img.style.visibility = "hidden";
   }
 }, 300);
+
+document.addEventListener("DOMContentLoaded", function () {
+  const popup = document.getElementById("popup");
+  const closeBtn = document.getElementById("close-btn");
+  const blurWrapper = document.getElementById("blur-wrapper");
+
+  popup.style.display = "flex";
+  blurWrapper.classList.add("blur-background");
+  closeBtn.addEventListener("click", function () {
+    popup.style.display = "none";
+    blurWrapper.classList.remove("blur-background");
+  });
+});
